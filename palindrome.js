108d1ef6b01d9442996d050dc6a1432a31216993
anand-divi <akhil244978@gmail.com>
2022-10-19 12:29:00 +0530

    prime and palindrome

---
+++ palindrome.js
@@ -0,0 +1,18 @@
+
+
+var str="ababa";
+
+var ans="";
+
+for(i=str.length;i>=0;i++){
+
+ans+=str[i];
+
+}
+
+if(str==ans){
+console.log(str,"is palindrome);
+}
+
+else
+console.log(str,"isn't palindrome);
---
+++ prime.js
@@ -0,0 +1,22 @@
+
+var num=13;
+
+var a=Math.sqrt(num);
+
+var cnt=0;
+
+       for(i=2;i<=a;i++){
+
+             if(num%i!=0){
+                  cnt++;
+                         }
+
+           }
+
+if(cnt==0&&num!=1)
+{
+console.log(num,"is prime);
+}
+
+else
+console.log(num,"isn't prime);
